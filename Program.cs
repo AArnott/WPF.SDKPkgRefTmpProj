196539a7b9ce7a4108c0e15a4d00ef644ba0e104
Andrew Arnott <andrewarnott@gmail.com>
2018-05-02 08:48:07 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpfsdk
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Console.WriteLine(ThisAssembly.AssemblyFileVersion);
+        }
+    }
+}
